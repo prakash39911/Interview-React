ecf751b8ef0d128f8a907a36c0ff5b7736fc3601
chandra prakash <prakash39911@gmail.com>
2025-07-07 13:57:33 +0530

    final code

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -10,6 +10,7 @@ import { cardStore } from "@/lib/store/cardStore";
 import { dateFormatter } from "@/lib/utilityFunction";
 import { CardDataType } from "@/types/type";
 import { X } from "lucide-react";
+import Image from "next/image";
 
 export default function CardComponent({ data }: { data: CardDataType }) {
   const removeCard = cardStore((state) => state.removeCard);
@@ -18,7 +19,7 @@ export default function CardComponent({ data }: { data: CardDataType }) {
   console.log("Total card--", cards.length);
 
   return (
-    <Card className="w-full max-w-sm">
+    <Card className="w-full max-w-sm flex flex-col h-[550px]">
       <CardHeader>
         <div className="flex justify-end">
           <X
@@ -27,12 +28,25 @@ export default function CardComponent({ data }: { data: CardDataType }) {
           />
         </div>
       </CardHeader>
-      <CardContent className="flex flex-col gap-2">
-        <div className="text-2xl font-bold">{data.title}</div>
-        <div>{data.body.slice(0, 90)}</div>
+      <CardContent className="flex flex-col flex-grow h-full gap-4">
+        <div className="text-2xl font-bold h-[100px]">{data.title}</div>
+        <div className="flex flex-wrap">
+          <div>{data.body.slice(0, 80)}</div>
+          <span>...</span>
+        </div>
+
         <div>{JSON.stringify(dateFormatter(new Date()))}</div>
 
-        {/* <Image alt={"image"} src={""} width={300} height={200} /> */}
+        <div className="relative bottom-0 mt-auto">
+          <Image
+            alt={"image"}
+            src={
+              "https://plus.unsplash.com/premium_photo-1661877737564-3dfd7282efcb?q=80&w=1200&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            }
+            width={300}
+            height={200}
+          />
+        </div>
       </CardContent>
       <CardFooter className="flex-col gap-2"></CardFooter>
     </Card>
